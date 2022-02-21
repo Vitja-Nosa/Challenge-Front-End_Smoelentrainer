78038544a76a889c02991027034b15671f367fe2
Vitja <99064218@mydavinci.nl>
2022-02-21 17:49:01 +0100

    minimale functionaliteit zit erin

---
+++ movies.js
@@ -0,0 +1,74 @@
+const movies = [
+    {
+        id: 1,
+        name: 'Agent Smith',
+        movie: 'The Matrix',
+        img: 'images/agent_smith.jpg'
+    },
+    {
+        id: 2,
+        name: 'Bruce Wayne',
+        movie: 'The Dark Knight',
+        img: 'images/bruce_wayne.jpg'
+    },
+    {
+        id: 3,
+        name: 'Calvin Candie',
+        movie: 'Django Unchained',
+        img: 'images/calvin_candie.png'
+    },
+    {
+        id: 4,
+        name: 'Cooper',
+        movie: 'Interstellar',
+        img: 'images/cooper.jpg'
+    },
+    {
+        id: 5,
+        name: 'David Mills',
+        movie: 'Se7en',
+        img: 'images/david_mills.jpg'
+    },
+    {
+        id: 6,
+        name: 'Frank Abagnale',
+        movie: 'Catch Me If You Can',
+        img: 'images/frank_abagnale.png'
+    },
+    {
+        id: 7,
+        name: 'Maximus',
+        movie: 'Gladiator',
+        img: 'images/maximus.jpg'
+    },
+    {
+        id: 8,
+        name: 'Norman Osborn',
+        movie: 'Spiderman',
+        img: 'images/norman_osborn.jpg'
+    },
+    {
+        id: 9,
+        name: 'Sherlock Holmes',
+        movie: 'Sherlock Holmes',
+        img: 'images/sherlock_holmes.jpg'
+    },
+    {
+        id: 10,
+        name: 'Truman',
+        movie: 'The Truman Show',
+        img: 'images/truman.jpg'
+    },
+    {
+        id: 11,
+        name: 'Walter White',
+        movie: 'Breaking Bad',
+        img: 'images/walter_white.jpg'
+    },
+    {
+        id: 12,
+        name: 'Patrick Bateman',
+        movie: 'American Psycho',
+        img: 'images/patrick_bateman.jpg'
+    }
+]
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const imgContainer = document.getElementById('imgContainer')
+const btnContainer = document.getElementById('btnContainer');
+const scoreElem = document.getElementById('score');
+const livesElem = document.getElementById('livesElem');
+
 const preGameContainer = document.getElementById('preGameContainer'),
 gameContainer = document.getElementById('gameContainer'),
 timer = document.getElementById('timer');
@@ -5,9 +10,20 @@ timer = document.getElementById('timer');
 const startBtn = document.getElementById('startBtn')
 startBtn.onclick = startGame;
 
+var lastClickedImg;
+var lastClickedBtn;
+
+var imgAmount = 10;
+var score = 0;
+var lives = 3;
+
 function startGame(){
     preGameContainer.style.display = 'none';
     gameContainer.style.display = 'block'
+    livesElem.innerText = `Attempts left: ${lives}`;
+    shuffle(movies, 10)
+    usedMovies = movies.splice(0,imgAmount)
+    createElements(usedMovies);
 }
 
 function startTimer(time) {
@@ -30,4 +46,80 @@ function startTimer(time) {
     }, 1000)
 }
 
-startTimer(30)
+function shuffle(arr, amt) {
+    for (j=0; j<amt; j++) {
+        for (i=0; i<arr.length; i++) {
+            randInt = Math.floor(Math.random() * arr.length);
+            temp = arr[i]
+            arr[i] = arr[randInt];
+            arr[randInt] = temp;
+        }
+    }
+}
+
+function createElements(arr) {
+    var qty = arr.length;
+    shuffle(arr, 20)
+    // creating the images
+    for (i=0; i<qty; i++) {
+        var elem = document.createElement('img');
+        elem.src = arr[i]['img'];
+        elem.dataset.id = arr[i]['id'] // giving id so we can match with btns later
+        elem.onclick = function() {
+            lastClickedImg = this; // remembering the last img we clicked for matching
+            addLastClickedStyling(imgContainer, 'selectedImg', this)
+            checkMatch()
+        }
+        imgContainer.appendChild(elem);
+    }
+    shuffle(arr, 20) // shuffling array so it's not the same pattern as imgs
+    // creating the buttons
+    for (i=0; i<qty; i++) {
+        var elem = document.createElement('button');
+        elem.innerText = arr[i]['movie'];
+        elem.dataset.id = arr[i]['id']; // same id as matching img
+        elem.classList = 'btn btn-success';
+        elem.onclick = function() {
+            lastClickedBtn = this
+            addLastClickedStyling(btnContainer, 'selectedBtn', this)
+            checkMatch()
+            
+        }
+        btnContainer.appendChild(elem)
+    }
+}
+
+function checkMatch() {
+    if (lastClickedImg != undefined && lastClickedBtn != undefined) {
+        if (lastClickedBtn.dataset.id == lastClickedImg.dataset.id) {
+            score++
+            scoreElem.innerText = `Score: ${score}`;
+            lastClickedBtn.remove();
+            lastClickedImg.remove();
+            document.body.style.backgroundColor = '#90EE90';
+            
+        }
+        else {
+            lives--
+            livesElem.innerText = `Attempts left: ${lives}`;
+            lastClickedImg.classList.remove('selectedImg')
+            lastClickedBtn.classList.remove('selectedBtn')
+            document.body.style.backgroundColor = '#FF7F7F';
+        }
+        setTimeout(() => {
+            document.body.style.backgroundColor = 'white';
+        },300)
+        lastClickedImg = lastClickedBtn = undefined
+       
+        
+        
+    }
+}
+function addLastClickedStyling(container, className, that) {
+    for (i=0; i<container.children.length; i++) {
+        crntElem = container.children[i];
+        crntElem.classList.remove(className);
+    }
+    that.classList.add(className)
+}
+startTimer(60)
